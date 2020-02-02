2bba22550f0bfcd27b0db0074224bb5275fa1595
alexus <afidelo@protonmail.com>
2020-02-03 00:50:35 +0100

    created basic ethereum service

--- microservice/src/app.controller.ts
+++ microservice/src/app.controller.ts
@@ -1,11 +1,19 @@
 import { Controller } from '@nestjs/common';
-import { EventPattern } from '@nestjs/microservices';
+import { EventPattern, MessagePattern } from '@nestjs/microservices';
+import { Block } from 'web3-eth/types';
+import { Observable, from } from 'rxjs';
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(){}
+  constructor(private readonly appService:AppService){}
   @EventPattern('echo')
   handleEchoMessage(data: string) {
     console.log('Microservice received data: ' + data);
   }
+  @MessagePattern('BLOCK')
+  public block(block:Block): Observable<Object>{
+    return from(block.transactions);
+  }
+
 }
--- microservice/src/main.ts
+++ microservice/src/main.ts
@@ -1,12 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
+//import { AnotherModule } from './another.module';
 
 async function bootstrap() {
+  
   const app = await NestFactory.createMicroservice(AppModule, {
-    transport: Transport.TCP,
-    //options: { host: 'localhost', port: 3002 }
+  //transport: Transport.TCP,
+  options: { host: 'localhost', port: 3002 }
   });
-   app.listen(() => console.log('First microservice I have ever deployed is listening'));
+
+  //const anotherApp = await NestFactory.createMicroservice(AnotherModule);
+
+  app.listen(() => console.log('First microservice I have ever deployed is listening'));
+  //anotherApp.listen(() => console.log('First microservice I have ever deployed is listening'));
+
 }
 bootstrap();
