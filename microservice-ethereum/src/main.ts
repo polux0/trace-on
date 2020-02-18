ec2a06a25526a8d226b936db126ba675e8a4d75f
alexus <afidelo@protonmail.com>
2020-02-18 23:11:44 +0100

    necessary changes

--- microservice-ethereum/src/main.ts
+++ microservice-ethereum/src/main.ts
@@ -5,7 +5,8 @@ import { EthereumServer } from './ethereum.server';
 async function bootstrap() {
   
   const app = await NestFactory.createMicroservice(AppModule, {
-    strategy: new EthereumServer()
+    strategy: new EthereumServer(),
+    options: {host: 'localhost', port: 3002}
   });
   await app.listen(() => console.log('First microservice I have ever deployed is listening'));
 
