7b0e258e97db212adb973c013b2fb3aec8f8e8f0
alexus <afidelo@protonmail.com>
2020-02-21 20:58:29 +0100

    purging unused imorts

--- microservice-client/src/app.controller.ts
+++ microservice-client/src/app.controller.ts
@@ -1,7 +1,6 @@
 
 import { Controller, Get, Inject } from '@nestjs/common';
 import {Observable, from, observable} from 'rxjs';
-import { AppService } from './app.service';
 import { ClientProxy } from '@nestjs/microservices';
 
 @Controller()
@@ -20,13 +19,13 @@ export class AppController {
   }
   @Get('blocks')
   getBlocks(): string {
-     const response = this.client.send<any>('blocks', new String('Requesting blocks...'));
+     const response = this.client.send<String>('blocks', new String('Requesting blocks...'));
      response.subscribe(console.log)
      return 'Message: `Subscribed to upcoming blocks.`';
   }
   @Get('transactions')
   getTransactions(): string {
-    const response = this.client.send<any>('transactions', new String('Requesting transactions...'));
+    const response = this.client.send<String>('transactions', new String('Requesting transactions...'));
     response.subscribe(console.log)
     return 'Message: `Subscribed to upcoming transactions.`'
   }
