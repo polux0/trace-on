88ad5ef8a53c07c862756258391679df38242ceb
alexus <afidelo@protonmail.com>
2020-03-04 00:20:16 +0100

    removing unused import

--- database-api/src/app.module.ts
+++ database-api/src/app.module.ts
@@ -2,11 +2,9 @@ import { Module, Inject, Injectable } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Neo4jService } from './neo4j.service';
-import { BlockModule } from './block/block.module';
-import { TransactionModule } from './transaction/transaction.module';
 
 @Module({
-  imports: [BlockModule, TransactionModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService, Neo4jService],
 })
