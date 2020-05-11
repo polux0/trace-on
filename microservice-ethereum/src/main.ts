5d840c7c8bd75f910f1932125ea395add53b3b34
alexus <afidelo@protonmail.com>
2020-05-11 23:56:06 +0200

    temporary setup and Docker

--- microservice-ethereum/src/main.ts
+++ microservice-ethereum/src/main.ts
@@ -3,14 +3,19 @@ import { AppModule } from './app.module';
 import { Transport } from '@nestjs/common/enums/transport.enum';
 
 async function bootstrap() {
-  
+
+  // kafka to be introduced
+  // const app = await NestFactory.createMicroservice(AppModule, {
+  //   transport: Transport.KAFKA,
+  //   options: {
+  //     client: {
+  //       brokers:['localhost:9092']
+  //   }
+  // }
+  // });
+  // testing if docker is set up right
   const app = await NestFactory.createMicroservice(AppModule, {
-    transport: Transport.KAFKA,
-    options: {
-      client: {
-        brokers:['localhost:9092']
-    }
-  }
+    options: {host: 'localhost', port: 4000}
   });
   await app.listen(() => console.log('Ethereum microservice is deployed and is listening'));
 
