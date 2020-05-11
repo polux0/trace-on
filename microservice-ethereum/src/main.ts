dee37db2fa87a0f9e350cd3cfa45fb090230912f
alexus <afidelo@protonmail.com>
2020-05-11 23:47:53 +0200

    minor

--- microservice-ethereum/src/main.ts
+++ microservice-ethereum/src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/common/enums/transport.enum';
 
 async function bootstrap() {
   
   const app = await NestFactory.createMicroservice(AppModule, {
-    options: {host: 'localhost', port: 4000}
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers:['localhost:9092']
+    }
+  }
   });
   await app.listen(() => console.log('Ethereum microservice is deployed and is listening'));
 
