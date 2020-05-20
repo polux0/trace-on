244b8a4aab036317df6b7c8f157799bdbebb6c60
alexus <afidelo@protonmail.com>
2020-05-20 23:46:26 +0200

    trying to bootstrap microservices via kafka

--- microservice-ethereum/src/main.ts
+++ microservice-ethereum/src/main.ts
@@ -5,18 +5,18 @@ import { Transport } from '@nestjs/common/enums/transport.enum';
 async function bootstrap() {
 
   // kafka to be introduced
-  // const app = await NestFactory.createMicroservice(AppModule, {
-  //   transport: Transport.KAFKA,
-  //   options: {
-  //     client: {
-  //       brokers:['localhost:9092']
-  //   }
-  // }
-  // });
-  // testing if docker is set up right
   const app = await NestFactory.createMicroservice(AppModule, {
-    options: {host: 'microservice-ethereum', port: 4000}
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers:['kafka']
+    }
+  }
   });
+  // testing if docker is set up right
+  // const app = await NestFactory.createMicroservice(AppModule, {
+  //   options: {host: 'microservice-ethereum', port: 4000}
+  // });
   await app.listen(() => console.log('Ethereum microservice is deployed and is listening'));
 
 }
