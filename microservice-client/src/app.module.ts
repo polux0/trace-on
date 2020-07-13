11f524bf3ebd018b109eb50648350ef8a6d6fd31
alexus <afidelo@protonmail.com>
2020-07-13 23:41:57 +0200

    connecting client

--- microservice-client/src/app.module.ts
+++ microservice-client/src/app.module.ts
@@ -7,7 +7,21 @@ import { Transport, ClientsModule } from '@nestjs/microservices'
 @Module({
 
   imports: [
-    ClientsModule.register([{name: 'ECHO_SERVICE', transport: Transport.TCP, options:{host:'microservice-ethereum', port: 4000}}]),
+    ClientsModule.register([
+      {
+        name: 'ECHO_SERVICE', 
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: 'ethereum-client',
+            brokers: ['trace-on_kafka_1:9092']
+          },
+          consumer: {
+            groupId: 'ethereum-client-consumer'
+          }
+        }
+      }
+    ]),
   ],
   controllers: [AppController],
   providers: [AppService],
